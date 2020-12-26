e2975716dcda3e2e19990656f8ff9973a7838eb7
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2020-12-26 16:15:39 +0000

    consider is_recur checkbox for trigger

--- js/recurbutton.js
+++ js/recurbutton.js
@@ -3,15 +3,22 @@
   if ($('#is_recur').size() == 0) {
     return;
   }
-
   if (($('#frequency_unit').size() > 0) && ($('input[name=is_recur_radio]').size() > 0)) {
-    $('#frequency_unit').on('change', function() {
-      var rbtnInterval = $('input[name=is_recur_radio]:checked').val();
-      var freqUnit = $('#frequency_unit').val();
-      if (freqUnit && (freqUnit != rbtnInterval)) {
-        $("input[name=is_recur_radio][value=" + freqUnit + "]").prop('checked', true);
+    $('#is_recur').change(function() {
+      if ($(this).is(":checked")) {
+        setRecurButton();
       }
     });
+    $('#frequency_unit').on('change', function() {
+      setRecurButton();
+    });
+  }
+  function setRecurButton() {
+    var rbtnInterval = $('input[name=is_recur_radio]:checked').val();
+    var freqUnit = $('#frequency_unit').val();
+    if (freqUnit && (freqUnit != rbtnInterval)) {
+      $("input[name=is_recur_radio][value=" + freqUnit + "]").prop('checked', true);
+    }
   }
 
 })(CRM.$, CRM._, CRM.ts('recurbutton'));
