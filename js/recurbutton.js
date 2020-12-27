85ef06d42918bc465a29d24bc8b2c2cd215f4551
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2020-12-27 15:31:17 +0000

    fixes to make it work based on staging site

--- js/recurbutton.js
+++ js/recurbutton.js
@@ -5,20 +5,17 @@
   }
   if (($('#frequency_unit').size() > 0) && ($('input[name=is_recur_radio]').size() > 0)) {
     $('#is_recur').change(function() {
-      if ($(this).is(":checked")) {
+      if ($('#is_recur').is(":checked")) {
         setRecurButton();
       }
     });
-    $('#frequency_unit').on('change', function() {
-      setRecurButton();
-    });
-  }
-  function setRecurButton() {
-    var rbtnInterval = $('input[name=is_recur_radio]:checked').val();
-    var freqUnit = $('#frequency_unit').val();
-    if (freqUnit && (freqUnit != rbtnInterval)) {
-      $("input[name=is_recur_radio][value=" + freqUnit + "]").prop('checked', true);
+    function setRecurButton() {
+      var rbtnInterval = $('input[name=is_recur_radio]:checked').val();
+      var freqUnit = $('#frequency_unit').val();
+      if (!freqUnit && !rbtnInterval) {
+        $("input[name=is_recur_radio][value=month]").prop('checked', true).trigger('change');
+      }
     }
   }
 
-})(CRM.$, CRM._, CRM.ts('recurbutton'));
+})(CRM.$, CRM._, CRM.ts('iskconcustom'));
